255e95faa7224a13b180c4a796795acaabfd63ed
Balja <balja@Uchkis-MacBook-Pro.local>
2022-12-13 07:19:56 +0800

    home

--- day20/interval.js
+++ day20/interval.js
@@ -2,13 +2,15 @@ let rootElement = document.getElementById("root");
 newDiv = document.createElement("div");
 rootElement.appendChild(newDiv);
 newDiv.style.width = "50px";
-let nemehWidth = "10px";
+
 newDiv.style.height = "50px";
 newDiv.style.border = "1px solid black";
-
+let width = 0;
 let myInterval = setInterval(function () {
-  newDiv.style.width++;
+  width++;
+  newDiv.style.width = width + "10px";
+
   if ((boxWidth = "500px")) {
     clearInterval(myInterval);
   }
-}, 100);
+}, 1000);
