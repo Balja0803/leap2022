421283a14bf6abadc5bddff7f6d8e07adea14233
balja0803 <balja0803@gmail.com>
2022-12-13 12:32:16 +0800

    school

--- day20/interval.js
+++ day20/interval.js
@@ -1,11 +1,21 @@
 let rootElement = document.getElementById("root");
 newDiv = document.createElement("div");
+textDiv = document.createElement("div");
+newDiv.appendChild(textDiv);
+document.getElementsByTagName("body")[0].style.maxWidth = "800px";
+let body = document.getElementsByTagName("body")[0];
 rootElement.appendChild(newDiv);
-newDiv.style.width = "50px";
-let width = "";
-setInterval(function () {
-  width++;
-  newDiv.style.width += width;
-}, 100);
 newDiv.style.height = "50px";
 newDiv.style.border = "1px solid black";
+newDiv.style.color = "white";
+
+let count = 0;
+let myInterval = setInterval(function () {
+  count++;
+  newDiv.style.width = `50%`;
+  newDiv.innerText = `r(${count}), g (${count}), b (${count})`;
+  newDiv.style.backgroundColor = `rgb(${count}, 30, 250)`;
+  if (count == "255") {
+    clearInterval(myInterval);
+  }
+}, 20);
