8f689b369c3574bc8d21b094e105d9ec241fa6d8
balja0803 <balja0803@gmail.com>
2022-12-13 08:45:45 +0800

    school

--- day20/interval.js
+++ day20/interval.js
@@ -1,10 +1,11 @@
 let rootElement = document.getElementById("root");
 newDiv = document.createElement("div");
 rootElement.appendChild(newDiv);
-
+newDiv.style.width = "50px";
+let width = "";
 setInterval(function () {
-  newDiv.style.width = "30px";
-  newDiv.style.width++;
-}, 1000);
+  width++;
+  newDiv.style.width += width;
+}, 100);
 newDiv.style.height = "50px";
 newDiv.style.border = "1px solid black";
