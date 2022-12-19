44ec8edefec32b7cd1523f11312bae3e3797f025
Balja <balja@Uchkis-MacBook-Pro.local>
2022-12-19 23:46:44 +0800

    home

--- callBack/function.js
+++ callBack/function.js
@@ -9,15 +9,3 @@ window.addEventListener("scroll", () => {
   let scrollPercentRounded = Math.round(scrollPercent * 100);
   percent.innerText = `${scrollPercentRounded}%`;
 });
-
-let names = [jam, jem, jen, jan];
-
-let arrForEach = (arr, cb) => {
-  for (let i = 0; i < arr.length; i++) {
-    let element = arr[i];
-    cb(element);
-  }
-};
-arrForEach(names, (name) => {
-  console.log(name);
-});
