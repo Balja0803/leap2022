285f912db0408fc712598966ce3782cc8ca60434
balja0803 <balja0803@gmail.com>
2022-12-23 12:07:56 +0800

    countries

--- fetchjson/script.js
+++ fetchjson/script.js
@@ -1,13 +1,10 @@
-const form = document.querySelector("form");
 const countriesContainer = document.querySelector(".countries");
 // https://restcountries.com/v3.1/name/${country}
 // https://restcountries.com/v3.1/alpha/${neighbour}
 
-getCountry("mongolia");
-
-function renderCountry(country) {
-  const html = `<article class="country">
-    <img class="country__img" src="${country.flag.svg}" />
+function renderCountry(country, className) {
+  const html = `<article class="country ${className}">
+    <img class="country__img" src="${country.flags.svg}" />
     <div class="country__data">
       <h3 class="country__name">${country.name.common}</h3>
       <h4 class="country__region">${country.region}</h4>
@@ -24,10 +21,29 @@ function renderCountry(country) {
   </article>`;
   countriesContainer.innerHTML += html;
   countriesContainer.style.opacity = 1;
+  console.log(country.borders[0]);
 }
 function getCountry(country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then((res) => res.json())
-    .then((data) => renderCountry(data[0]))
-    .then((data) => console.log(data));
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (neighbour) {
+        getNeighbour(neighbour);
+      }
+    });
+}
+// getCountry("united states");
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getCountry(`${event.target.country_name.value}`);
+});
+
+function getNeighbour(country) {
+  fetch(`https://restcountries.com/v3.1/alpha/${country}`)
+    .then((res) => res.json())
+    .then((data) => renderCountry(data[0], "neighbour"));
 }
