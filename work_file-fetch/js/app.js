705983ceeceb4e7338b93b832c55ab68ede27711
balja0803 <balja0803@gmail.com>
2022-12-26 12:32:49 +0800

    es6

---
+++ work_file-fetch/js/app.js
@@ -0,0 +1,29 @@
+const select = document.getElementById("breeds");
+const card = document.querySelector(".card");
+fetch("https://dog.ceo/api/breeds/list")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    generateBreeds(data.message);
+  });
+
+function generateBreeds(data) {
+  data.map((dogBreeds) => {
+    const html = `<option value ="${dogBreeds}">${dogBreeds}</option> `;
+    select.innerHTML += html;
+  });
+}
+
+function generateImage(url) {
+  const image = `<img src="${url}">`;
+  card.innerHTML += html;
+}
+
+function fetchImage(breedType) {
+  fetch(`https://dog.ceo/api/breed/${breedType}/images/random`)
+    .then((res) => res.json())
+    .then((data) => generateImage(data.message));
+}
+card.addEventListener("click", () => {
+  console.log("clicked");
+});
