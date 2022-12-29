960fe5323fc6fccc98746026e1878d4797a3137f
balja0803 <balja0803@gmail.com>
2022-12-29 12:27:03 +0800

    school
    sdf

--- bank/function.js
+++ bank/function.js
@@ -3,15 +3,50 @@ document.getElementsByTagName("body")[0].style.backgroundColor = "grey";
 const getUsers = document.createElement("div");
 root.appendChild(getUsers);
 getUsers.innerHTML = "<button>Get users</button>";
-
+const inputYear = document.createElement("div");
+root.appendChild(inputYear);
+inputYear.innerHTML =
+  "<input id='yearInput' type='number'></input><button id='yearBtn'>Submit</button>";
+let yearInput = document.getElementById("yearInput");
+let yearBtn = document.getElementById("yearBtn");
 getUsers.addEventListener("click", () => {
   fetch("http://localhost:3333/api/users")
     .then((res) => res.json())
     .then((res) => {
-      res.map((user) => {
-        let userInfo = document.createElement("div");
-        root.appendChild(userInfo);
-        userInfo.innerHTML = `<div>${user.id}</div`;
+      res.map((user, index) => {
+        let table = document.createElement("div");
+        root.appendChild(table);
+        table.style.border = "1px solid black";
+
+        let interestRate = function (isTrue) {
+          if (user.balance_type) {
+            return "10%";
+          } else {
+            return "1%";
+          }
+        };
+        yearBtn.addEventListener("click", () => {
+          let totalVal = document.querySelector("#totalValue");
+          // input value-g boolean-s hamaarch urjuuleh
+          totalVal.innerHTML = yearInput.value;
+        });
+        table.innerHTML = `
+  <table>
+  <tr>
+  <th>id</th>
+  <th>name</th>
+  <th>balance</th>
+  <th>Interest Rate</th>
+  <th> Total </th>
+  </tr>
+  <tr>
+  <td>${user.id}</td>
+  <td>${user.name}, ${user.surName}</td>
+  <td>${user.balance} $</td>
+  <td>${interestRate(user.balance_type)}</td>
+  <td id='totalValue'></td>
+  </tr>
+  </table>`;
       });
     });
 });
